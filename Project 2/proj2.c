9cc4b217e36877c8a47b80df236f9ac90e021cc4
vdtstorm <51832333+vdtstorm@users.noreply.github.com>
2020-11-01 16:09:58 -0500

    Add files via upload

--- Project 2/proj2.c
+++ Project 2/proj2.c
@@ -9,8 +9,18 @@
 #define NUMMEMORY 65536 /* maximum number of words in memory */
 #define NUMREGS 8 /* number of machine registers */
 #define MAXLINELENGTH 1000
+// Instruction Definitions
+#define ADD 0
+#define NAND 1
+#define LW 2
+#define SW 3
+#define BEQ 4
+#define JALR 5
+#define HALT 6
+#define NOOP 7
 
-typedef struct stateStruct {
+typedef struct stateStruct 
+{
     int pc;
     int mem[NUMMEMORY];
     int reg[NUMREGS];
@@ -20,8 +30,7 @@ typedef struct stateStruct {
 int convertNum(int num);
 void printState(stateType *);
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     char line[MAXLINELENGTH];
     stateType state;
@@ -55,30 +64,34 @@ main(int argc, char *argv[])
 	int count = 0;
 	int temp  = 0;
 	int offset = 0;
-	bool beq = false;
-	for(int i = 0; i< 8; i++)
+	bool halt = false;
+	int regA = 0, regB = 0, regC = 0;
+	// Setting regs to zero
+	for(int i = 0; i < 8; i++)
         	state.reg[i] = 0;
-	while((state.mem[state.pc] >> 22)!=6) // Goes until halt is seen
+	while((state.mem[state.pc] >> 22) != HALT) // Goes until halt is seen
 	{
-		int regA = 0, regB = 0, regC = 0;
-		count++;
+		
 		printState(&state);
-		if((state.mem[state.pc] >> 22) == 0) // Check for ADD instruction
+		if((state.mem[state.pc] >> 22) == ADD) // Check for ADD instruction
 		{
 			regA = ((state.mem[state.pc] >> 19) & 7);
 			regB = ((state.mem[state.pc] >> 16) & 7);
 			regC = (state.mem[state.pc] & 7);
 			state.reg[regC] = regA + regB;
+			
 		}
-		if((state.mem[state.pc] >> 22) == 1) // Check for NAND instruction
+
+		if((state.mem[state.pc] >> 22) == NAND) // Check for NAND instruction
         	{
                 	regA = ((state.mem[state.pc] >> 19) & 7);
                 	regB = ((state.mem[state.pc] >> 16) & 7);
                 	regC = (state.mem[state.pc] & 7);
 			state.reg[regC] = ~(regA & regB);
+			
         	}
 		
-		if((state.mem[state.pc] >> 22) == 2) // Check for LW instruction
+		if((state.mem[state.pc] >> 22) == LW) // Check for LW instruction
                 {
                         regA = ((state.mem[state.pc] >> 19) & 7);
                         regB = ((state.mem[state.pc] >> 16) & 7);
@@ -86,9 +99,10 @@ main(int argc, char *argv[])
 			temp = convertNum(regC);
 			offset = temp + state.reg[regA];
 			state.reg[regB] = state.mem[offset];
+			
                 }
 		
-		if((state.mem[state.pc] >> 22) == 3) // Check for SW instruction
+		if((state.mem[state.pc] >> 22) == SW) // Check for SW instruction
                 {
                 	regA = ((state.mem[state.pc] >> 19) & 7);
                         regB = ((state.mem[state.pc] >> 16) & 7);
@@ -96,42 +110,54 @@ main(int argc, char *argv[])
                         temp = convertNum(regC);
                         offset = temp + state.reg[regA];
                         state.mem[offset] = state.reg[regB];
+			
                 }
-		/*
-		if((state.mem[state.pc] >> 22) == 4) // Check for BEQ instruction
+		
+		if((state.mem[state.pc] >> 22) == BEQ) // Check for BEQ instruction
                 {
                         regA = ((state.mem[state.pc]  >> 19) & 7);
                         regB = ((state.mem[state.pc]  >> 16) & 7);
                         regC = (state.mem[state.pc] & 0xFFFF);
 			temp = convertNum(regC);
-                        if(regA == regB)
+			
+                        // if regA == regB, PC = PC + 1 + OFFSET
+			/*
+			if(regA == regB)
 			{
 				state.pc = state.pc + 1 + temp;
-				beq = true;	
+				
 			}
+			else
+			*/
+			
+			
+	
                 }
-		
-		if((state.mem[state.pc] >> 22) == 5) // Check for JALR instruction
+		/*
+		else if((state.mem[state.pc] >> 22) == JALR) // Check for JALR instruction
                 {
                         regA = ((state.mem[state.pc] >> 19) & 7);
-
                         regB = ((state.mem[state.pc] >> 16) & 7);
-			if(regA == regB)
+			if(state.reg[regA] == state.reg[regB])
                         {
                                 state.pc = state.pc + regA;
-
                         }
+			state.pc++;
+			
                     
                 }
 		*/
-		if(beq==false)
-			state.pc++;
+		state.pc++;
+		count++;
+	
+		
 	}
+	
 	printf("machine halted\n");
 	printf("total of %d instructions executed\n", count);
 	printf("final state of machine:\n");
 	printState(&state);
-    return(0);
+    return 0;
 }
 
 void printState(stateType *statePtr)
