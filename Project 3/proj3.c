b5e41e3e97b615eaaa378c752ed4476d47d10f7c
vdtstorm <51832333+vdtstorm@users.noreply.github.com>
2020-11-02 18:21:08 -0500

    Add files via upload

--- Project 3/proj3.c
+++ Project 3/proj3.c
@@ -1,4 +1,3 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -6,6 +5,7 @@
 
 #define NUMMEMORY 65536 /* maximum number of data words in memory */
 #define NUMREGS 8 /* number of machine registers */
+#define MAXLINELENGTH 1000
 
 #define ADD 0
 #define NAND 1
@@ -68,82 +68,139 @@ int field1(int instruction);
 int field2(int instruction);
 int opcode(int instruction);
 void printInstruction(int instr);
+void IF(const stateType, stateType*);
+void ID(const stateType, stateType*);
+void EX(const stateType, stateType*);
+void MEM(const stateType, stateType*);
+void WB(const stateType, stateType*);
+
 
 int main(int argc, const char *argv[])
 {
-        stateType state, newState;
-        while (1) {
+	// Code from reading in file comes from project 2
+	stateType state, newState;
+	char line[MAXLINELENGTH];
+    
+    	FILE *filePtr;
 
-                printState(&state);
+    	if (argc != 2) 
+	{
+        	printf("error: usage: %s <machine-code file>\n", argv[0]);
+        	exit(1);
+    	}
 
-        /* check for halt */
-        if (opcode(state.MEMWB.instr) == HALT) {
-            printf("machine halted\n");
-            printf("total of %d cycles executed\n", state.cycles);
-            exit(0);
-        }
+    	filePtr = fopen(argv[1], "r");
+    	if (filePtr == NULL) 
+	{
+        	printf("error: can't open file %s", argv[1]);
+        	perror("fopen");
+        	exit(1);
+    	}
 
-        newState = state;
-        newState.cycles++;
+    	/* read in the entire machine-code file into memory */
+    	for (state.numMemory = 0; fgets(line, MAXLINELENGTH, filePtr) != NULL;	
+	state.numMemory++) 
+	{
+        	if (sscanf(line, "%d", state.instrMem+state.numMemory) != 1) 
+		{
+            		printf("error in reading address %d\n", state.numMemory);
+            		exit(1);
+        	}
+        	printf("memory[%d]=%d\n", state.numMemory, state.instrMem[state.numMemory]);
+	}
 
-        /* --------------------- IF stage --------------------- */
+	// Setting pipline instructions to Noop instructions
+	state.pc = 0;
+	state.cycles = 0;
 
-        /* --------------------- ID stage --------------------- */
+	
+	for(int i = 0; i < NUMREGS; i++) 
+	{
+		state.reg[i] = 0;
+	}
 
-        /* --------------------- EX stage --------------------- */
+	// NOOPs
+	state.IFID.instr = NOOPINSTRUCTION;
+	state.IDEX.instr = NOOPINSTRUCTION;
+	state.EXMEM.instr = NOOPINSTRUCTION;
+	state.MEMWB.instr = NOOPINSTRUCTION;
+	state.WBEND.instr = NOOPINSTRUCTION;
 
-        /* --------------------- MEM stage --------------------- */
+	while (1) 
+	{
 
-        /* --------------------- WB stage --------------------- */
+		printState(&state);
 
-        state = newState; /* this is the last statement before end of the loop.
-                            It marks the end of the cycle and updates the
-                            current state with the values calculated in this
-                            cycle */
-    }
+		/* check for halt */
+		if (opcode(state.MEMWB.instr) == HALT) 
+		{
+	    		printf("machine halted\n");
+	    		printf("total of %d cycles executed\n", state.cycles);
+	    		exit(0);
+		}
 
-        return 0;
+		newState = state;
+		newState.cycles++;
+
+		/* --------------------- IF stage --------------------- */
+
+		/* --------------------- ID stage --------------------- */
+
+		/* --------------------- EX stage --------------------- */
+
+		/* --------------------- MEM stage --------------------- */
+
+		/* --------------------- WB stage --------------------- */
+
+		state = newState; /* this is the last statement before end of the loop.
+			    		It marks the end of the cycle and updates the
+			    		current state with the values calculated in this
+			    		cycle 
+				*/
+    	}
+
+	return 0;
 }
 
 void printState(stateType *statePtr)
 {
     int i;
     printf("\n@@@\nstate before cycle %d starts\n", statePtr->cycles);
-    printf("\tpc %d\n", statePtr->pc); 
-    
-     printf("\tdata memory:\n");
-        for (i=0; i<statePtr->numMemory; i++) {
-            printf("\t\tdataMem[ %d ] %d\n", i, statePtr->dataMem[i]);
-        }
+    printf("\tpc %d\n", statePtr->pc);
+
+    printf("\tdata memory:\n");
+	for (i=0; i<statePtr->numMemory; i++) {
+	    printf("\t\tdataMem[ %d ] %d\n", i, statePtr->dataMem[i]);
+	}
     printf("\tregisters:\n");
-        for (i=0; i<NUMREGS; i++) {
-            printf("\t\treg[ %d ] %d\n", i, statePtr->reg[i]);
-        }
+	for (i=0; i<NUMREGS; i++) {
+	    printf("\t\treg[ %d ] %d\n", i, statePtr->reg[i]);
+	}
     printf("\tIFID:\n");
-        printf("\t\tinstruction ");
-        printInstruction(statePtr->IFID.instr);
-        printf("\t\tpcPlus1 %d\n", statePtr->IFID.pcPlus1);
+	printf("\t\tinstruction ");
+	printInstruction(statePtr->IFID.instr);
+	printf("\t\tpcPlus1 %d\n", statePtr->IFID.pcPlus1);
     printf("\tIDEX:\n");
-        printf("\t\tinstruction ");
-        printInstruction(statePtr->IDEX.instr);
-        printf("\t\tpcPlus1 %d\n", statePtr->IDEX.pcPlus1);
-        printf("\t\treadRegA %d\n", statePtr->IDEX.readRegA);
-        printf("\t\treadRegB %d\n", statePtr->IDEX.readRegB);
-        printf("\t\toffset %d\n", statePtr->IDEX.offset);
+	printf("\t\tinstruction ");
+	printInstruction(statePtr->IDEX.instr);
+	printf("\t\tpcPlus1 %d\n", statePtr->IDEX.pcPlus1);
+	printf("\t\treadRegA %d\n", statePtr->IDEX.readRegA);
+	printf("\t\treadRegB %d\n", statePtr->IDEX.readRegB);
+	printf("\t\toffset %d\n", statePtr->IDEX.offset);
     printf("\tEXMEM:\n");
-         printf("\t\tinstruction ");
-        printInstruction(statePtr->EXMEM.instr);
-        printf("\t\tbranchTarget %d\n", statePtr->EXMEM.branchTarget);
-        printf("\t\taluResult %d\n", statePtr->EXMEM.aluResult);
-        printf("\t\treadRegB %d\n", statePtr->EXMEM.readRegB);
+	printf("\t\tinstruction ");
+	printInstruction(statePtr->EXMEM.instr);
+	printf("\t\tbranchTarget %d\n", statePtr->EXMEM.branchTarget);
+	printf("\t\taluResult %d\n", statePtr->EXMEM.aluResult);
+	printf("\t\treadRegB %d\n", statePtr->EXMEM.readRegB);
     printf("\tMEMWB:\n");
-        printf("\t\tinstruction ");
-        printInstruction(statePtr->MEMWB.instr);
-        printf("\t\twriteData %d\n", statePtr->MEMWB.writeData);
+	printf("\t\tinstruction ");
+	printInstruction(statePtr->MEMWB.instr);
+	printf("\t\twriteData %d\n", statePtr->MEMWB.writeData);
     printf("\tWBEND:\n");
-        printf("\t\tinstruction ");
-        printInstruction(statePtr->WBEND.instr);
-        printf("\t\twriteData %d\n", statePtr->WBEND.writeData);
+	printf("\t\tinstruction ");
+	printInstruction(statePtr->WBEND.instr);
+	printf("\t\twriteData %d\n", statePtr->WBEND.writeData);
 }
 
 int field0(int instruction)
@@ -170,30 +227,26 @@ void printInstruction(int instr)
 {
     char opcodeString[10];
     if (opcode(instr) == ADD) {
-        strcpy(opcodeString, "add");
+	strcpy(opcodeString, "add");
     } else if (opcode(instr) == NAND) {
-        strcpy(opcodeString, "nand");
+	strcpy(opcodeString, "nand");
     } else if (opcode(instr) == LW) {
-        strcpy(opcodeString, "lw");
+	strcpy(opcodeString, "lw");
     } else if (opcode(instr) == SW) {
-        strcpy(opcodeString, "sw");
+	strcpy(opcodeString, "sw");
     } else if (opcode(instr) == BEQ) {
-        strcpy(opcodeString, "beq");
+	strcpy(opcodeString, "beq");
     } else if (opcode(instr) == JALR) {
-        strcpy(opcodeString, "jalr");
+	strcpy(opcodeString, "jalr");
     } else if (opcode(instr) == HALT) {
-        strcpy(opcodeString, "halt");
+	strcpy(opcodeString, "halt");
     } else if (opcode(instr) == NOOP) {
-        strcpy(opcodeString, "noop");
+	strcpy(opcodeString, "noop");
     } else {
-        strcpy(opcodeString, "data");
+	strcpy(opcodeString, "data");
     }
 
     printf("%s %d %d %d\n", opcodeString, field0(instr), field1(instr),
-        field2(instr));
+	field2(instr));
 }
-
-
-    
-
 
